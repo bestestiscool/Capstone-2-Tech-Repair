15dbaccd3c72059d9255b66386d1fbf33692b6d2
bestestiscool <Daniel_Makalski13@hotmail.ca>
2024-09-27 10:04:17 -0400

    set up supabase db with our local code

--- models/index.js
+++ models/index.js
@@ -1,19 +1,27 @@
-'use strict';
-
 const fs = require('fs');
 const path = require('path');
 const Sequelize = require('sequelize');
 const process = require('process');
 const basename = path.basename(__filename);
 const env = process.env.NODE_ENV || 'development';
-const config = require(__dirname + '/../config/config.json')[env];
+const config = require('../config/config.js')[env];  // Ensure you are selecting the right environment config
 const db = {};
 
 let sequelize;
 if (config.use_env_variable) {
   sequelize = new Sequelize(process.env[config.use_env_variable], config);
 } else {
-  sequelize = new Sequelize(config.database, config.username, config.password, config);
+  sequelize = new Sequelize(
+    config.database,
+    config.username,
+    config.password,
+    {
+      host: config.host,
+      dialect: config.dialect,  // Ensure the dialect is passed here
+      port: config.port,
+      dialectOptions: config.dialectOptions || {},
+    }
+  );
 }
 
 fs
