f77f9aeacd2f5ac31cae9a0e081f20816e0fab73
bestestiscool <Daniel_Makalski13@hotmail.ca>
2024-09-30 15:19:27 -0400

    update to remove attempt 2

--- client/src/App.js
+++ client/src/App.js
@@ -5,9 +5,6 @@ import YouTubeVideos from './components/YouTubeVideos';
 import VideoDetails from './components/VideoDetails';
 import RepairCostEstimator from './components/RepairCostEstimator';
 import ProjectsGithub from './components/Projects-Github';
-import ProtectedRoute from './components/ProtectedRoute';
-// import Login from './components/Login';  // NEW IMPORT
-// import Register from './components/Register';  // NEW IMPORT
 
 const API_KEY = process.env.REACT_APP_YOUTUBE_API_KEY;
 const channelId = process.env.REACT_APP_CHANNEL_ID;
@@ -23,7 +20,7 @@ function App() {
             <Route path="/shorts" element={<YouTubeVideos API_KEY={API_KEY} channelId={channelId} videoType="short" />} />
             <Route path="/long-videos" element={<YouTubeVideos API_KEY={API_KEY} channelId={channelId} videoType="long" />} />
             <Route path="/video/:id" element={<VideoDetails API_KEY={API_KEY} />} />
-            <Route path="/repair-cost-estimator" element={<ProtectedRoute component={RepairCostEstimator} />} />
+            <Route path="/repair-cost-estimator" element={<RepairCostEstimator />} />
             <Route path="/portfolio" element={<ProjectsGithub />} />
           </Routes>
         </header>
