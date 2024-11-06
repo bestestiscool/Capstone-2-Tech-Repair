e1604898dbf52d3877f1cee1d324a8d26108d0bd
bestestiscool <Daniel_Makalski13@hotmail.ca>
2024-11-05 20:48:56 -0500

    so far I changed the icon of the page and name

--- client/src/App.js
+++ client/src/App.js
@@ -14,7 +14,7 @@ function App() {
   return (
     <Router>
       <div className="App">
-        {/* <Navbar /> */}
+        <Navbar />
         <header className="App-header">
           <Routes>
             <Route path="/" element={<HomePage API_KEY={API_KEY} channelId={channelId} videoType="all" />} />
